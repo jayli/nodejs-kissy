e94c1b088b006e0f92e19978827f3f08752c4174
bachi <bachi@Taobao-uedbj.taobao.ali.com>
2010-11-03 17:16:54 +0800

    new files

---
+++ cli.js
@@ -0,0 +1,32 @@
+#!/usr/bin/env node
+
+var yui3 = require('yui3-core');
+
+process.chdir(yui3.path() + '/api/');
+
+console.log(yui3.path());
+
+try {
+    var express = require('express');
+} catch (e) {
+    console.log('Could not require expressjs; install via: npm install express');
+    process.exit();
+}
+var args = process.argv.slice(2),
+arg, key, config = { port: 8100 };
+
+while (arg = args.shift()) {
+    if ("--" === arg.substr(0, 2)) {
+        key = arg.substr(2).split('=');
+        config[key[0]] = key[1];
+    }
+}
+
+var app = express.createServer();
+app.configure(function() {
+    app.use(express.staticProvider(process.cwd()));
+});
+var port = parseInt(config.port);
+app.listen(port);
+console.log('YUI3 API Docs are available:');
+console.log('http://localhost:' + port);
